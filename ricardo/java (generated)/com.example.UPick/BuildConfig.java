ea1ebeae31e601c5f7a9ca4045018809d54225ba
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-04-28 21:35:55 -0700

    Update BuildConfig.java

--- ricardo/java (generated)/com.example.UPick/BuildConfig.java
+++ ricardo/java (generated)/com.example.UPick/BuildConfig.java
@@ -1,31 +1,15 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
 package com.example.upick;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
-
-public class SelectionScreen extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_selection_screen);
-
-        configurePickAgainButton();
-    }
-
-    private void configurePickAgainButton(){
-        Button backButton = (Button)findViewById(R.id.pickagain);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
-    }
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.upick";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
 }
+
 
