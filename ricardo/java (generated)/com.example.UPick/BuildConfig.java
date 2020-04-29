7687d2ba2721afffdf9218a097337218e578996c
Bear-cop <60299988+Bear-cop@users.noreply.github.com>
2020-04-28 21:34:22 -0700

    Update BuildConfig.java

--- ricardo/java (generated)/com.example.UPick/BuildConfig.java
+++ ricardo/java (generated)/com.example.UPick/BuildConfig.java
@@ -1,1 +1,31 @@
+package com.example.upick;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class SelectionScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_selection_screen);
+
+        configurePickAgainButton();
+    }
+
+    private void configurePickAgainButton(){
+        Button backButton = (Button)findViewById(R.id.pickagain);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
 
